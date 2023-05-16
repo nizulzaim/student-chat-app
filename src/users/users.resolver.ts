3a62c0081f255d21705235f654553e9090369e31
Nizul Zaim <mnizulzaim@gmail.com>
2023-05-17 03:31:41 +0800

    feat: test commit

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -10,4 +10,9 @@ export class UsersResolver {
   users() {
     return this.userService.users();
   }
+
+  @Query(() => [User])
+  async user() {
+    return (await this.userService.users())[0];
+  }
 }
